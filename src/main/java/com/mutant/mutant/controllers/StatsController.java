a407dafd777cd03b089705001c796224c673eec3
TomyFernandez <tomas.fernandez277@gmail.com>
2024-10-28 17:37:36 -0300

    Primer Commit

--- src/main/java/com/mutant/mutant/controllers/StatsController.java
+++ src/main/java/com/mutant/mutant/controllers/StatsController.java
@@ -2,6 +2,10 @@ package com.mutant.mutant.controllers;
 
 import com.mutant.mutant.dtos.StatsResponse;
 import com.mutant.mutant.services.StatsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,17 +14,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="*")
+@CrossOrigin(origins = "*")
 @RequestMapping("/stats")
 public class StatsController {
 
     private final StatsService statsService;
 
     @Autowired
-    public StatsController(StatsService statsService){
+    public StatsController(StatsService statsService) {
         this.statsService = statsService;
     }
 
+    @Operation(summary = "Obtener estadísticas de verificaciones de ADN")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Estadísticas obtenidas con éxito", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Error interno del servidor", content = @Content)
+    })
     @GetMapping
     public ResponseEntity<StatsResponse> getStats() {
         StatsResponse stats = statsService.getStats();
